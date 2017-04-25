d6338a49ceb23e8ada8cbbb235529ace6b49aa76
Céline MS <celine.martinet.sanchez+github@gmail.com>
2017-04-25 17:08:48 +0200

    tests ok

--- program/download_agents.py
+++ program/download_agents.py
@@ -5,33 +5,37 @@ import time
 import urllib.error
 import urllib.request
 
+def get_agents(count):
+    # le parse
+    agents = []
+    while len(agents) < count:
+        if agents:
+            # Wait one second between every request
+            time.sleep(1)
+
+        request_count = min(count - len(agents), 500)
+        try:
+            response = urllib.request.urlopen("http://pplapi.com/batch/{}/sample.json".format(request_count))
+            agents += json.loads(response.read().decode("utf8"))
+        except urllib.error.HTTPError:
+            print("Too many requests, sleeping 10s ({} agents)".format(len(agents)))
+            time.sleep(10)
+    return agents
+
 
 def main():
     parser = argparse.ArgumentParser(description="Download agents from pplapi.com")
     parser.add_argument("-c", "--count", type=int, default=10, help="Number of agents to download.")
     parser.add_argument("-d", "--dest", help="Destination file. If absent, will print to stdout")
-    args = parser.parse_args()
+    args = parser.parse_args(args.count)
 
-    agents = []
-    while len(agents) < args.count:
-        if agents:
-            # Wait one second between every request
-            time.sleep(1)
-
-        request_count = min(args.count - len(agents), 500)
-        try:
-            response = urllib.request.urlopen("http://pplapi.com/batch/{}/sample.json".format(request_count))
-            print(response)
-            agents += json.loads(response.read().decode("utf8"))
-        except urllib.error.HTTPError:
-            print("Too many requests, sleeping 10s ({} agents)".format(len(agents)))
-            time.sleep(10)
+    agents = get_agents(args.count)
 
     result = json.dumps(agents, indent=2, sort_keys=True)
     if args.dest:
         pass
-        #with open(args.dest, 'w') as out_f:
-            #out_f.write(result)
+        with open(args.dest, 'w') as out_f:
+            out_f.write(result)
     else:
         print(result)
 
--- test_world.py
+++ test_world.py
@@ -1,17 +1,5 @@
-from program.world import *
+import program.world as script
 import pytest
-
-def setup_function(function):
-    """ setup any state tied to the execution of the given function.
-    Invoked for every test function in the module.
-    """
-    pass
-
-def teardown_function(function):
-    """ teardown any state that was previously setup with a setup_function
-    call.
-    """
-    pass
     
 def hello(name):
     return 'Hello ' + name
@@ -25,22 +13,28 @@ def test_hello():
 #############################################
 
 class TestAgent:
-    AGENTTEST = Agent(3)
+    @classmethod
+    def setup_class(cls):
+        cls.AGENT = script.Agent(3)
+
+    @classmethod
+    def teardown_class(cls):
+        del(cls.AGENT)
     # - Agent : 
     #   - récupérer un attribut position
     def test_set_position(self):
-        self.AGENTTEST.position = 5
-        assert self.AGENTTEST.position == 5
+        self.AGENT.position = 5
+        assert self.AGENT.position == 5
 
     #   - modifier un attribut position
     def test_get_position(self):
-        assert self.AGENTTEST.position == 5
+        assert self.AGENT.position == 5
 
     #   - assigner un dictionnaire en tant qu'attributs
     def test_set_agent_attributes(self):
-        dictionary = {"agreeableness": -0.8437190198916452}
-        agent = Agent(3, **dictionary)
-        assert agent.agreeableness == -0.8437190198916452
+        dictionary = {"agreeableness": -1}
+        agent = script.Agent(3, **dictionary)
+        assert agent.agreeableness == -1
 
 
 #############################################
@@ -48,33 +42,33 @@ class TestAgent:
 #############################################
 
 class TestPosition:
-    POSITIONTEST = Position(100.85840672174572, 33.15219798270325)
+    POSITIONTEST = script.Position(100, 33)
     # - Position :
     #   - modifier un attribut longitude_degrees
     def test_longitude_degrees(self):
-        assert self.POSITIONTEST.longitude_degrees == 100.85840672174572
+        assert self.POSITIONTEST.longitude_degrees == 100
 
     #   - modifier un attribut latitude_degrees
     def test_latitude_degrees(self):
-        assert self.POSITIONTEST.latitude_degrees == 33.15219798270325
+        assert self.POSITIONTEST.latitude_degrees == 33
 
-    #   - modifier un attribut longitude_degrees avec une valeur supérieure à 180 renvoie une erreur. 
+    #   - modifier un attribut longitude_degrees avec une valeur supérieure à 180 renvoie une erreur.
     def test_longitude_degrees_range(self):
         with pytest.raises(AssertionError):
-            position = Position(200.85840672174572, 33.15219798270325)
+            script.Position(200, 33)
 
-    #   - modifier un attribut latitude_degrees avec une valeur supérieure à 90 renvoie une erreur. 
+    #   - modifier un attribut latitude_degrees avec une valeur supérieure à 90 renvoie une erreur.
     def test_latitude_degrees_range(self):
         with pytest.raises(AssertionError):
-            position = Position(100.85840672174572, 100.15219798270325)
+            script.Position(100, 100)
 
     #   - récupérer une longitude
     def test_longitude(self):
-        assert self.POSITIONTEST.longitude == 1.7603112756100432
+        assert self.POSITIONTEST.longitude == 1.7453292519943295
 
     #   - récupérer une latitude
     def test_latitude(self):
-        assert self.POSITIONTEST.latitude == 0.5786150090711938
+        assert self.POSITIONTEST.latitude == 0.5759586531581288
 
 
 #############################################
@@ -82,84 +76,104 @@ class TestPosition:
 #############################################
 
 class TestZone:
-    POSITION1 = Position(100, 33)
-    POSITION2 = Position(101, 34)
-    ZONETEST = Zone(POSITION1, POSITION2)
-    AGENT = Agent(POSITION1, **{"agreeableness": -0.8437190198916452})
 
-    # - Zone :
-    #   - récupérer un coin inférieur gauche (corner1)
-    def test_zone_corner1(self):
-        assert self.ZONETEST.corner1 == self.POSITION1
+    @classmethod
+    def setup_class(cls):
+        cls.POSITION1 = script.Position(100, 33)
+        cls.POSITION2 = script.Position(101, 34)
+        cls.ZONE = script.Zone(cls.POSITION1, cls.POSITION2)
 
-    #   - récupérer un coin supérieur droit (corner2)
-    def test_zone_corner2(self):
-        assert self.ZONETEST.corner2 == self.POSITION2
+    @classmethod
+    def teardown_class(cls):
+        del(cls.POSITION1)
+        del(cls.POSITION2)
+        del(cls.ZONE)
+        script.Zone.ZONES = []
 
-    #   - trouver une zone qui contient une position
-    def test_find_zone_that_contains(self):
-        found_zone = Zone.find_zone_that_contains(self.POSITION1)
-        assert found_zone.corner1.longitude == self.ZONETEST.corner1.longitude
+    def setup_method(self):
+        script.Zone._initialize_zones()
+        agent = script.Agent(self.POSITION1, agreeableness=1)
+        self.ZONE.inhabitants = [agent]
 
-    #   - ajouter un habitant dans une zone
-    def test_add_inhabitant_in_zone(self):
-        init = self.ZONETEST.inhabitants
-        self.ZONETEST.add_inhabitant(self.AGENT)
-        assert len(self.ZONETEST.inhabitants) == 1
+    def teardown_method(self):
+        self.ZONES = []
+        #agent = script.Agent(self.POSITION1, agreeableness=1)
+        #self.ZONETEST.inhabitants = [agent]
 
     #   - récupérer toutes les instances Zone (Zone.ZONES)
     # On devrait avoir exactement 64800 zones
     def test_get_zones(self):
-        assert len(Zone.ZONES) == 64800
+        assert len(script.Zone.ZONES) == 64800
+
+    # - Zone :
+    def test_zone_that_contains(self):
+        position = self.POSITION1
+        found_zone = script.Zone.find_zone_that_contains(position)
+        assert position.longitude >= min(found_zone.corner1.longitude, found_zone.corner2.longitude)
+        assert position.longitude < max(found_zone.corner1.longitude, found_zone.corner2.longitude)
+        assert position.latitude >= min(found_zone.corner1.latitude, found_zone.corner2.latitude)
+        assert position.latitude < max(found_zone.corner1.latitude, found_zone.corner2.latitude)
+
+
+    #   - trouver une zone qui contient une position
+    def test_find_zone_that_contains(self):
+        found_zone = script.Zone.find_zone_that_contains(self.POSITION1)
+        assert found_zone.contains(self.POSITION1)
+
+    #   - ajouter un habitant dans une zone
+    def test_add_inhabitant_in_zone(self):
+        agent = script.Agent(self.POSITION1, agreeableness=1)
+        self.ZONE.add_inhabitant(agent)
+        assert len(self.ZONE.inhabitants) == 2
+
 
     #   - récupérer la densité de population d'une zone
     def test_get_population_density(self):
-        assert self.ZONETEST.population_density() == 8.087793508722422e-05
+        assert self.ZONE.population_density() == 8.087793508722422e-05
 
 #   - récupérer l'agréabilité moyenne d'une zone
     def test_get_average_agreeableness(self):
-        assert self.ZONETEST.average_agreeableness() == -0.8437190198916452
+        assert self.ZONE.average_agreeableness() == 1
 
 
 class TestAgreeablenessGraph:
-    TESTGRAPH = AgreeablenessGraph()
 # - AgreeablenessGraph :
+
+    @classmethod
+    def setup_class(cls):
+        script.Zone._initialize_zones()
+        cls.ZONE = script.Zone.ZONES[0]
+        cls.GRAPH = script.AgreeablenessGraph()
+        cls.ZONES = script.Zone.ZONES
+        for _ in range(0, 10):
+            cls.ZONE.add_inhabitant(script.Agent(script.Position(-180, -89), agreeableness=1))
+
+    @classmethod
+    def teardown_class(cls):
+        del(cls.GRAPH)
+        del(cls.ZONE)
+        del(cls.ZONES)
 
 #   - récupérer un titre
     def test_title(self):
-        assert self.TESTGRAPH.title == 'Nice people live in the countryside'
+        assert self.GRAPH.title == 'Nice people live in the countryside'
 
 #   - récupérer x_label
     def test_x_label(self):
-        assert self.TESTGRAPH.x_label == 'population density'
+        assert self.GRAPH.x_label == 'population density'
 
 #   - récupérer y_label
     def test_y_label(self):
-        assert self.TESTGRAPH.y_label == 'agreeableness'
+        assert self.GRAPH.y_label == 'agreeableness'
 
 #   - récupérer xy_values sous forme de tuples
     def test_xy_values(self):
-        pass
-        #assert len(self.XY_VALUES) == 2
+        assert len(self.GRAPH.xy_values(self.ZONES)) == 2
 
 #   - la première valeur de xy_values est la densité de population moyenne pour la première zone
     def test_average_population_density(self):
-        # Initialize zones
-        zone = Zone.ZONES[0]
-        graph = AgreeablenessGraph()
-        # # Add inhabitants
-        for _ in range(0, 10):
-            zone.add_inhabitant(Agent(Position(-180, -89), **{"agreeableness": -0.8437190198916452}))
-        a = graph.xy_values(Zone.ZONES)
-        assert graph.xy_values(Zone.ZONES)[0][0] == zone.population_density()
+        assert self.GRAPH.xy_values(self.ZONES)[0][0] == self.ZONE.population_density()
 
 #   - la seconde valeur de xy_values est l'agréabilité moyenne
     def test_average_agreeableness(self):
-        # Initialize zones
-        zone = Zone.ZONES[0]
-        graph = AgreeablenessGraph()
-        # # Add inhabitants
-        for _ in range(0, 10):
-            zone.add_inhabitant(Agent(Position(-180, -89), **{"agreeableness": -0.8437190198916452}))
-        a = graph.xy_values(Zone.ZONES)
-        assert graph.xy_values(Zone.ZONES)[1][0] == zone.average_agreeableness()
+        assert self.GRAPH.xy_values(self.ZONES)[1][0] == self.ZONE.average_agreeableness()
